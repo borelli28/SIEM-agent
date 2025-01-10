a86a66c6790d1c0a69a8dd5d98de37f28cc353fe
borelli <borelliarmando28@gmail.com>
2025-01-10 01:46:33 -0500

    Added AgentConfig data on agent registration on main.rs

--- cef_agent/src/main.rs
+++ cef_agent/src/main.rs
@@ -42,16 +42,26 @@ async fn register_agent(host_id: String, account_id: String, hostname: String)
     let registration = AgentRegistration {
         id: String::new(),
         api_key: String::new(),
-        host_id,
-        account_id,
+        host_id: host_id.clone(),
+        account_id: account_id.clone(),
         ip_address: Some("127.0.0.1".to_string()),
         hostname: Some(hostname),
         status: "Active".to_string()
     };
 
     let response = client.register(registration).await?;
-    println!("Registration successful! Agent ID: {}", response.agent_id);
-    
+    println!("Registration successful!");
+
+    let config = AgentConfig {
+        agent_id: response.agent_id.clone(),
+        api_key: response.api_key.clone(),
+        host_id: host_id.clone(),
+        account_id: account_id.clone(),
+        watch_paths: Vec::new(),
+        siem_url: "http://localhost:4200".to_string(),
+    };
+    config.save()?;
+
     Ok(response)
 }
 
